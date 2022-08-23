d5716bf0005d0178747506a1c607f8e2d75816ba
Facundo <coconaut95@gmail.com>
2022-08-23 13:15:14 -0300

    Application finished

--- books.py
+++ books.py
@@ -2,7 +2,7 @@
 # Dive into FastAPI, a web framework for developing modern RESTful APIs using Python. 
 # FastAPI is built off of Starlette and Pydantic, therefore the performance of FastAPI is off the charts. 
 # Pydantic allows type hints to perform data validation and serialization / deserialization of data. 
-# astAPI automatically creates interactive Swagger UI documentation which is perfect for any developer. 
+# FastAPI automatically creates interactive Swagger UI documentation which is perfect for any developer. 
 #################################
 
 #Here i'll create created FastAPI
@@ -23,10 +23,10 @@ models.Base.metadata.create_all(bind=engine) #when this line run, it create the 
 # This funtion handle the open and close of the DB
 def get_db(): 
     try:
-        db = SessionLocal
+        db = SessionLocal()
         yield db
     finally:
-        db.close
+        db.close()
 
 # Schema
 class Book(BaseModel):    
@@ -35,15 +35,15 @@ class Book(BaseModel):
     description: str = Field(min_length=1, max_length=100)
     rating: int = Field(gt=-1, lt=101) # between 0 and 100
 
-BOOKS = [] #it can be replace with a DB
+# BOOKS = [] #it can be replace with a DB
 
 
 @app.get('/') #'/{}' path parameter
-def read_api(db: Session = Depends(get_db)):
+def read_all_books(db: Session = Depends(get_db)):
     return db.query(models.Books).all()
 
 @app.post('/')
-def create_books(book:Book, db: Session = Depends(get_db)):   
+def create_book(book:Book, db: Session = Depends(get_db)):   
 
     book_model = models.Books() #Mimic the model "Books"
     book_model.title = book.title
@@ -57,27 +57,36 @@ def create_books(book:Book, db: Session = Depends(get_db)):
     return book
 
 @app.put('/{book_id}')
-def update_book(book_id: UUID, book:Book):
-    counter = 0
-    for x in BOOKS:
-        counter+=1
-        if x.id == book_id:
-            BOOKS[counter - 1] = book
-            return BOOKS[counter - 1]
-    raise HTTPException(
-        status_code=404,
-        detail=f"ID {book_id}: Does not exist"
-    )
+def update_book(book_id: int, book:Book, db: Session = Depends(get_db)):
+    
+    book_model = db.query(models.Books).filter(models.Books.id == book_id).first() # It keep the first founded
+
+    if book_model is None:
+        raise HTTPException(
+            status_code=404,
+            detail=f"ID {book_id}: Does not exist"
+        )    
+    
+    book_model.title = book.title
+    book_model.author = book.author
+    book_model.description = book.description
+    book_model.rating = book.rating
+
+    db.add(book_model)
+    db.commit()
+
+    return book
 
 @app.delete('/{book_id}')
-def delete_book (book_id: UUID):
-    counter = 0
-    for x in BOOKS:
-        counter +=1
-        if x.id == book_id:
-            del BOOKS[counter - 1]
-            return f"ID: {book_id} delated"
-    raise HTTPException(
-        status_code=404,
-        detail=f"ID {book_id}: Does not exist"
-    )
+def delete_book (book_id: int, book:Book, db: Session = Depends(get_db)):
+
+    book_model = db.query(models.Books).filter(models.Books.id == book_id).first() 
+    # Verification
+    if book_model is None:
+        raise HTTPException(
+            status_code=404,
+            detail=f"ID {book_id}: Does not exist"
+        )
+    
+    db.query(models.Books).filter(models.Books.id == book_id).delete()
+    db.commit()
